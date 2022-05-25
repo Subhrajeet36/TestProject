48bed47f0fce1128af1f9f5b426a3ee762990f62
subhrajeet.mitra <subhrajeet.mitra@BLRKEC555242L.ad.infosys.com>
2022-05-25 15:52:55 +0530

    First commit

---
+++ erf/src/main/java/dfgfdg/erf/App.java
@@ -0,0 +1,14 @@
+package dfgfdg.erf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+x`
